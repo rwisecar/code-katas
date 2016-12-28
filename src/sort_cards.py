e7e54936f1cf52f0e72cdb5a2c20fc12a4206f5d
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-28 09:39:46 -0800

    refactoring sort_cards function to remove unnecessary looping

--- src/sort_cards.py
+++ src/sort_cards.py
@@ -5,8 +5,4 @@ def sort_cards(cards):
     """Sorts a deck of cards by their value, with Aces low."""
     rank = {"A": 0, "2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7,
             "9": 8, "T": 9, "J": 10, "Q": 11, "K": 12}
-    sorted_list = []
-    for c in cards:
-        sorted_list.append((c, rank[c]))
-    sorted_list = [x[0] for x in sorted(sorted_list, key=lambda x: x[1])]
-    return sorted_list
+    return sorted(cards, key=lambda x: rank[x])
